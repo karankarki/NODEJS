d9f8a8eddc9d38e5d537778d55fc6b5fdc37cb2a
karan singh karki <78966718+karankarki@users.noreply.github.com>
2024-01-04 15:05:47 +0530

    Add files via upload

---
+++ 5_reading_comnd_line_input.js
@@ -0,0 +1,28 @@
+// 1.Import ReadLine
+
+
+// const readline  = require('readline');
+
+
+// // 2.Confuigre interface to connect with terminal/Command line
+
+// const interface = readline.createInterface(
+//     {
+//         input:process.stdin,
+//         output:process.stdout,
+//     }
+// )
+
+// // 3. Reading values
+
+//  const r =interface.question("Enter the first number ",(num1)=>{
+//     interface.question("Enter the second number ",(num2)=>{
+//         // sum and return 
+//         console.log(Number(num1)+Number(num2));
+
+//     })
+//  });
+
+//  console.log("hello");
+
+var s1 = getline();
