217296e79e911df6e64053a6269b93fc65ee503d
karan singh karki <78966718+karankarki@users.noreply.github.com>
2024-01-15 12:46:09 +0530

    Update 1_Blockin_non_blocking.js

--- 1_Blockin_non_blocking.js
+++ 1_Blockin_non_blocking.js
@@ -1,10 +1,10 @@
 
 setTimeout(()=>{
     for(var i = 0;i<1000000000;i++){
-        //Nothing
+     console.log("Done 0");
     }
     console.log("DONE 1");
-},0)
+},0);
 
 
 console.log("hehehhehehe 2");
